f4b6a75bdbeea491f7555fd08ece7bae93e8b91d
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-25 15:48:50 -0500

    feat: add singleAlbum component

---
+++ client/components/SingleAlbum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Album from './Album'
+
+const SingleAlbum = () => {
+  return ( 
+    <div id="single-album" className="column">
+      <Album />
+    </div>
+  );
+}
+ 
+export default SingleAlbum;
