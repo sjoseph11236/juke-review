ebf8ab61f48895cbc78753faa4cf6cca67a3aa18
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-25 12:30:23 -0500

    feat: create GET albums route

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,15 @@
 const router = require('express').Router()
+const { Album } = require('../db/index')
 
 // connect your API routes here!
+router.get('/albums', async(req, res, next) =>  {
+  try {
+    const allAlbums = await Album.findAll();
+    res.status(200).send(allAlbums);
+  } catch (error) {
+    console.log('This is the error ', error);
+    next(error);
+  }
+})
 
 module.exports = router
--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,21 @@
-const db = require('./db')
+const { db, Artist, Album, Song } = require('./db')
 // require each of your models here...
 
 // ...and give them some nice associations here!
+Song.belongsTo(Album);
+Album.hasMany(Song);
+
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
+
+Album.belongsTo(Artist);
+Artist.hasMany(Album);
 
 module.exports = {
   db,
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
+  Artist, 
+  Album, 
+  Song
 }
