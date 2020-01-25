796e8f1f0ac89d34ef1d9ca8abe4bbef13c9b752
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-01-25 14:51:39 -0500

    feat: add icons to playerContainer

--- client/Main.js
+++ client/Main.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import SideBar  from './components/SideBar';
+import Container from './components/Container'; 
+import PlayContainer from './components/PlayContainer'
+
 
 export default class Main extends React.Component {
   render () {
     return (
       <div id='main' className='row container'>
         {/* The music starts here! */}
+        {/* Sidebar */}
+        <SideBar />
+        <Container />
+        {/* player-container */}
+        <PlayContainer/>
       </div>
     )
   }
---
+++ client/components/SideBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SideBar = () => {
+  return ( 
+    <div id="sidebar">
+      <img src="juke.svg" alt="logo" id='logo'/>
+      <section>
+        <h4>
+          <a href="">ALBUMS</a>
+        </h4>
+      </section>
+    </div>
+  );
+}
+
+export default SideBar;
